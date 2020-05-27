a1b108a04b94347138b5639c1af3c641e3adcd74
paddymills <pmiller1389@gmail.com>
2020-05-27 12:48:17 -0400

    set version to pre-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hss_core',
-    version=1.0,
+    version=0.1,
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
