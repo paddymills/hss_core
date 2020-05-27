8cb68d5bee836f3690d71977d0eb9ceb37e457ae
paddymills <pmiller1389@gmail.com>
2020-05-27 10:34:15 -0400

    update settings with dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='hss_core', version=1.0, packages=find_packages())
+setup(
+    name='hss_core',
+    version=1.0,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "xlwings",
+        "pyodbc",
+        "pandas",
+        "graphqlclient",
+        "inflection"
+    ]
+)
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from os.path import join, dirname
-
-# file handler imports
-import hss_core.io
