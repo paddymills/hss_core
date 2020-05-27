2adfc773230ad500798c3f509c4869be8e76a3f4
paddymills <pmiller1389@gmail.com>
2020-05-27 10:41:28 -0400

    add setup dependency versions

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "xlwings",
-        "pyodbc",
-        "pandas",
-        "graphqlclient",
-        "inflection"
+        "xlwings>=0.19.4",
+        "pyodbc>=4.0.30",
+        "pandas>=1.0.3",
+        "graphqlclient>=0.2.4",
+        "inflection>=0.4.0"
     ]
 )
