83b9c4be0e382971c9f35922bfeb8bead274a6db
paddymills <pmiller1389@gmail.com>
2020-06-05 10:36:43 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prodctrlcore',
-    version="0.5.1",
+    version="0.5.2-beta",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
